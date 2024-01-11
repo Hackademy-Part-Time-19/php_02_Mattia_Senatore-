df7097aa87d5a73792eb78fb0013d3155d6c44cc
MattiaSen <147874985+MattiaSen@users.noreply.github.com>
2024-01-11 19:10:39 +0100

    secondo

--- php.php
+++ php.php
@@ -1,9 +1,25 @@
 <?php
 
+$users = [['name' => 'Davide', 'surname' => 'Cariola', 'gender' => 'M'],
+['name' => 'Giorgia', 'surname' => 'Bianchi', 'gender' => 'F'],
+['name' => 'Optimus', 'surname' => 'Prime', 'gender' => 'NB']
+];
+
+//for ($i = 0; $i < count($users); $i++){}
+    
 
 
-
-
+foreach($users as $elemento){
+    if ($elemento['gender']=='M') {
+        echo "{$elemento['name']} {$elemento['surname']} è di genere Maschile.\n";
+    }
+    if ($elemento['gender']=='F') {
+        echo "{$elemento['name']} {$elemento['surname']} è di genere Femminile.\n";
+    }
+    if ($elemento['gender']=='NB') {
+        echo "{$elemento['name']} {$elemento['surname']} è di genere N.B.\n";
+    }
+}
 
 
 
